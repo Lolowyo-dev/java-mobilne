a3d07eba8454abb74e9942ff8b3fd4e5c8fae675
Kacper Chamala <98347889+Lolowyo-dev@users.noreply.github.com>
2023-10-10 09:09:42 +0000

    fajnie

--- PAM/zad5/Main.java
+++ PAM/zad5/Main.java
@@ -5,7 +5,7 @@ class pole_prostokata{
     double a,b,pole;
 
     public void czytaj_dane()
-    throws Exception
+    throws IOException // Buffer reader nie moze zczytac
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Program oblicza pole prostokata");
@@ -20,18 +20,18 @@ class pole_prostokata{
     }
 
     public void wyswietl_wynik(){
-        System.out.println("Pole prostokata o boku a = ");
+        System.out.print("Pole prostokata o boku a = ");
         System.out.printf("%2.2f", a);
-        System.out.println("i boku b = ");
+        System.out.print(" i boku b = ");
         System.out.printf("%2.2f", b);
-        System.out.println(" wynosi ");
+        System.out.print(" wynosi ");
         System.out.printf("%2.2f.\n", pole);
     }
 }
 
 public class Main{
     public static void main(String[] args)
-    throws Exception
+    throws IOException
     {
         pole_prostokata pole = new pole_prostokata();
 
