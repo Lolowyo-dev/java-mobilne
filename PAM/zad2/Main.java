1e74641406a70590e6621fdd811eda1a3ec602e3
Kacper Chamala <98347889+Lolowyo-dev@users.noreply.github.com>
2023-10-10 08:18:50 +0000

    zad2

---
+++ PAM/zad2/Main.java
@@ -0,0 +1,14 @@
+package PAM.zad2;
+
+public class Main {
+    
+    public static void main(String[] args){
+        char znak;
+
+        for(int i=0; i<=255; i++){
+            znak = (char)i;
+            System.out.println(i+" - "+znak);
+        }
+    }
+}
+
