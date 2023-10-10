d8df4f6dc2170b48c6d38d5b260ef3a55bc374e4
Kacper Chamala <98347889+Lolowyo-dev@users.noreply.github.com>
2023-10-10 09:04:20 +0000

    zad5

---
+++ PAM/zad5/Main.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Scanner;
+
+class pole_prostokata{
+    double a,b,pole;
+
+    public void czytaj_dane()
+    throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Program oblicza pole prostokata");
+        System.out.print("Podaj bok a: ");
+        a = Double.parseDouble(br.readLine());
+        System.out.print("Podaj bok b: ");
+        b = Double.parseDouble(br.readLine());
+    }
+
+    public void przetworz_dane(){
+        pole = a*b;
+    }
+
+    public void wyswietl_wynik(){
+        System.out.println("Pole prostokata o boku a = ");
+        System.out.printf("%2.2f", a);
+        System.out.println("i boku b = ");
+        System.out.printf("%2.2f", b);
+        System.out.println(" wynosi ");
+        System.out.printf("%2.2f.\n", pole);
+    }
+}
+
+public class Main{
+    public static void main(String[] args)
+    throws Exception
+    {
+        pole_prostokata pole = new pole_prostokata();
+
+        pole.czytaj_dane();
+        pole.przetworz_dane();
+        pole.wyswietl_wynik();
+    }
+}
