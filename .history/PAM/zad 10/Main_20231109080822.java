ce21839c3620db867f1adcf8f6fa5b42a071eebb
Kacper Chamala <98347889+Lolowyo-dev@users.noreply.github.com>
2023-11-13 09:47:00 +0000

    asd

---
+++ .history/PAM/zad 10/Main_20231109080822.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner aha = new Scanner(System.in);
+        Double liczba;
+        String zn;
+        System.out.print("\nPodaj liczbe: ");
+        liczba = aha.nextDouble();
+
+        System.out.print("\nPodaj r jak radiany, s jak stopnie: ");
+        zn = aha.nextLine();
+
+        liczba = zn=='r' ? liczba*180/Math.PI : liczba*Math.PI/180;
+        
+        System.out.println("\nWynik wynosi: "+liczba);
+
+        aha.close();
+    }
+}
