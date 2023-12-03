47336a35668b6af23d214d9f98f61d4b0a753e89
Lolowyo <kacperchamala06@gmail.com>
2023-12-03 11:07:55 +0100

    sf

--- .history/PAM/zad9/Main_20231102081817.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        //Wczytaj z klawiatury 5 dowolnych liczb, wyswietl je a nastepnie odwroc kolejnosc w tablicy i wyswietl
-        Double tablica;
-    }
-}
--- .history/PAM/zad9/Main_20231102081905.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        //Wczytaj z klawiatury 5 dowolnych liczb, wyswietl je a nastepnie odwroc kolejnosc w tablicy i wyswietl
-        Double tablica;
-    }
-}
