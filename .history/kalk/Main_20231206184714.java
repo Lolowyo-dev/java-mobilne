098fe716db5ef3923b272b72d6341a08ed27ce71
Lolowyo <kacperchamala06@gmail.com>
2023-12-06 18:47:47 +0100

    aha

---
+++ .history/kalk/Main_20231206184714.java
@@ -0,0 +1,34 @@
+package kalk;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        // tworzenie FXMLLoader 
+        FXMLLoader loader = new FXMLLoader();
+
+        FileInputStream fxmlStream = new FileInputStream("asdqwe.fxml");
+ 
+        // Tworzy panel z pliku fxml
+        VBox root = (VBox) loader.load(fxmlStream);
+ 
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("A simple FXML Example");
+        stage.show();
+    }
+
+    public static void main(String[] args) 
+    {
+        Application.launch(args);
+    }
+}
